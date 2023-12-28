a821d3d3e5e9cc8a2c4aac1b5b4ff12717a473ed
achneerov <achneerov@gmail.com>
2023-12-28 00:38:33 -0500

    better

--- generate.py
+++ generate.py
@@ -1,23 +1,56 @@
-# generate.py
-
 import pickle
 import random
+
+GENOME_SIZE = 50  # Define the size of each genome
 
 population_per_generation = 10
 number_of_generations = 10
 mutation_rate = 0.1
 
+def fitness_function(model):
+    """Dummy fitness function: Returns the length of the model."""
+    return len(model)
+
+def crossover(parent1, parent2):
+    """Perform crossover between two parents."""
+    crossover_point = random.randint(0, GENOME_SIZE - 1)
+    child = parent1[:crossover_point] + parent2[crossover_point:]
+    return child
+
+def mutate(model):
+    """Mutate the model based on mutation_rate."""
+    for i in range(len(model)):
+        if random.random() < mutation_rate:
+            model[i] = random.choice(["w", "a", "s", "d"])
+    return model
 
 def generate_models(population_per_generation, number_of_generations, mutation_rate):
+    best_model = None
+
+    models = [[random.choice(["w", "a", "s", "d"]) for _ in range(GENOME_SIZE)] for _ in range(population_per_generation)]
+
     for generation in range(number_of_generations):
-        models = []
-        for _ in range(population_per_generation):
-            model = [random.choice(["UP", "DOWN", "LEFT", "RIGHT"]) for _ in range(GENOME_SIZE)]
-            models.append(model)
+        # Evaluate fitness for each model
+        fitness_values = [fitness_function(model) for model in models]
 
-        # Save all models for the current generation
-        with open(f"generation_{generation}_models.pkl", "wb") as f:
-            pickle.dump(models, f)
+        # Select models based on fitness (for simplicity, select the best half)
+        sorted_indices = sorted(range(len(fitness_values)), key=lambda k: fitness_values[k], reverse=True)
+        best_model = models[sorted_indices[0]]  # Get the best model
 
-        # For a real Genetic Algorithm, you'd evaluate fitness, select parents, perform crossover, mutate, etc.
-        # Here, for simplicity, we're just saving all models for each generation.
+        # Perform crossover and mutation to generate new models
+        new_models = []
+        while len(new_models) < population_per_generation:
+            parent1 = random.choice(models)
+            parent2 = random.choice(models)
+            child = crossover(parent1, parent2)
+            child = mutate(child)
+            new_models.append(child)
+
+        models = new_models
+
+    # Save the best model from the last generation
+    with open("best_model_from_last_generation.pkl", "wb") as f:
+        pickle.dump(best_model, f)
+
+# Generate models using the enhanced genetic algorithm approach
+generate_models(population_per_generation, number_of_generations, mutation_rate)
--- load.py
+++ load.py
@@ -1,22 +1,27 @@
-#load.py
-
 from game import Dot, WhiteDot, WIDTH, HEIGHT, WHITE_DOT_RADIUS, RED_DOT_RADIUS, NUMBER_OF_RED_DOTS, distance
 import pickle
 import random
+
 
 def play_game_with_model(model):
     # Initialize white dot
     white_dot = WhiteDot(WIDTH // 2, HEIGHT // 2)
     red_dots = [Dot(random.randint(0, WIDTH), random.randint(0, HEIGHT)) for _ in range(NUMBER_OF_RED_DOTS)]
 
-    for direction in model:
-        if direction == "UP":
+    # Debugging: Print the model to see its content
+    print(f"Model Directions: {model}")
+
+    index = 0  # Start from the first direction in the model
+    while index < len(model):
+        direction = model[index]
+
+        if direction == "w":
             white_dot.move_towards(white_dot.x, white_dot.y - 10)
-        elif direction == "DOWN":
+        elif direction == "s":
             white_dot.move_towards(white_dot.x, white_dot.y + 10)
-        elif direction == "LEFT":
+        elif direction == "a":
             white_dot.move_towards(white_dot.x - 10, white_dot.y)
-        elif direction == "RIGHT":
+        elif direction == "d":
             white_dot.move_towards(white_dot.x + 10, white_dot.y)
 
         # Check for collision and eat red dots
@@ -24,10 +29,13 @@ def play_game_with_model(model):
             if distance(white_dot.x, white_dot.y, red_dot.x, red_dot.y) < WHITE_DOT_RADIUS + RED_DOT_RADIUS:
                 red_dots.remove(red_dot)
 
+        index += 1  # Move to the next direction in the model
+
     return len(red_dots)
 
+
 # Load the models
-with open("generation_9_model.pkl", "rb") as f:
+with open("best_model_from_last_generation.pkl", "rb") as f:  # Corrected filename here
     model = pickle.load(f)
     remaining_red_dots = play_game_with_model(model)
     print(f"Remaining Red Dots: {remaining_red_dots}")
